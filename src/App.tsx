9ace2b19fc2b28289652e3686450c6950976b288
lebed2045 <lebed2045@protonmail.com>
2019-09-08 02:54:59 -0400

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,29 @@
 import React from 'react';
 import logo from './logo.svg';
+import logoNu from './logoNu.png';
 import './App.css';
 import {Encryptor} from "./Encryptor";
+import {Decryptor} from "./Decryptor";
+import { HashRouter as Router, Route, Link, Switch } from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
+    <div className="App" >
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Encryptor />
+        <img src={logoNu} className="App-logo" alt="logo" />
+          <Router>
+              <div style={{ maxWidth: "80vw" }}>
+                  <nav>
+                      <Link to="/">Encryptor</Link>
+                        <br/>
+                      <Link to="/decryptor">Decryptor</Link>
+                  </nav>
+                  <Switch>
+                      <Route exact path="/" component={Encryptor} />
+                      <Route exact path="/decryptor" component={Decryptor} />
+                  </Switch>
+              </div>
+          </Router>
       </header>
 
     </div>
--- src/Encryptor.tsx
+++ src/Encryptor.tsx
@@ -1,50 +1,63 @@
 import * as React from "react";
 import {useState} from "react";
-import {Button, InputGroup, Text, TextArea} from "@blueprintjs/core";
+import {Button, InputGroup, Text} from "@blueprintjs/core";
 import {Intent} from "@blueprintjs/core/lib/esm/common/intent";
 import Axios from "axios";
-
+// import { Example, handleStringChange, IExampleProps } from "@blueprintjs/docs-theme";
 
 export const Encryptor = () => {
-    const [address, setAddress] = useState();
-    const [message, setMessage] = useState();
-    const [result, setResult] = useState("");
+    const [publicKey, setPublicKey] = useState("a6bbc4aedd8019a67c9f9e4267c138062aed491070129faf8b108eacd0a6bb9f1d133b446a6904026680e5bfd6041cc95a4f3310ed1743de6157cc4fed796c66");
+    const [message, setMessage] = useState("message");
+    const [result1, setResult1] = useState("");
+    const [result2, setResult2] = useState("");
+    const [error, setError] = useState("");
 
     const encryptButtonHandler = async () => {
 
         const url = "http://167.99.255.241:5000/encrypt";
         const data = {
-            "recipient":"a6bbc4aedd8019a67c9f9e4267c138062aed491070129faf8b108eacd0a6bb9f1d133b446a6904026680e5bfd6041cc95a4f3310ed1743de6157cc4fed796c66",
-            "data":"data"
+            "recipient": publicKey,
+            "data": message
         };
         let axiosConfig = {
             headers: {
                 'Content-Type': 'application/json;charset=UTF-8',
-                "Access-Control-Allow-Origin":"*"
+                "Access-Control-Allow-Origin": "*"
             }
         };
 
-        const response = await Axios.post(url, data, axiosConfig);
-        console.log(response);
-        setResult(response.data);
+        try {
+            const response = await Axios.post(url, data, axiosConfig);
+            console.log(response);
+            setResult1(JSON.stringify(response.data.capsule));
+            setResult2(JSON.stringify(response.data.ciphertext));
+        } catch (e) {
+            setError(e.message);
+        }
+
     };
 
     return (
         <div>
+            <h2> Encryptor </h2>
             <p> Recipient </p>
-            <InputGroup placeholder={"address"}
-                        onChange={(event: React.ChangeEvent<HTMLInputElement>) => setAddress(event.target.value)}
+            <InputGroup placeholder={"publicKey"}
+                        onChange={(event: React.ChangeEvent<HTMLInputElement>) => setPublicKey(event.target.value)}
+                        value={publicKey}
             />
             <br/>
             <p> Message </p>
             <InputGroup placeholder={"enter message"}
                         onChange={(event: React.ChangeEvent<HTMLInputElement>) => setMessage(event.target.value)}
+                        value={message}
             />
             <br/>
             <Button intent={Intent.PRIMARY}
                     onClick={encryptButtonHandler}>encrypt</Button>
             <br/>
-            {result &&(<Text>{result}</Text>)}
+            {result1 && (<Text ellipsize={true}>{result1}</Text>)}
+            {result2 && (<Text ellipsize={true}>{result2}</Text>)}
+            {error && (<p style={{ color: "red" }}> error: {error}</p>)}
         </div>
     );
 };
